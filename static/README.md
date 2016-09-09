629fcbba7467ae51cbe26fec633c1a1342b0bf96
uborzz <uborzz@gmail.com>
2016-09-09 17:22:37 +0200

    add gulp task

---
+++ static/README.md
@@ -0,0 +1,7 @@
+
+npm install
+
+gulp css
+gulp js
+
+output en out/css y out/js
--- static/gulpfile.js
+++ static/gulpfile.js
@@ -1,5 +1,39 @@
-var jshint = require('gulp-jshint');
-var gulp   = require('gulp');
+var gulp = require('gulp'),
+ minifyCSS = require('gulp-minify-css'),
+ concatCss = require('gulp-concat-css'),
+ concatJs = require('gulp-concat'),
+ notify = require('gulp-notify'),
+ uglify = require('gulp-uglify');
+
+gulp.task('css', function ()
+{
+  gulp.src('*.css')
+    .pipe(concatCss("todo.css"))
+    .pipe(minifyCSS({keepBreaks:false}))
+    .pipe(gulp.dest('out/css'))
+    .pipe(notify("Ha finalizado la task css!"));
+});
+
+gulp.task('js', function()
+{
+  gulp.src('app/**/*.js')
+    .pipe(concatJs('concat.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('out/js'))
+    .pipe(notify("Ha finalizado la task js!"));
+});
+
+
+
+//
+//
+//var jshint = require('gulp-jshint');
+//var gulp   = require('gulp');
+//
+
+
+
+
 
 
 // gulp.task('default', function() {
